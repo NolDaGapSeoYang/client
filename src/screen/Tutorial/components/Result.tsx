c49810ef444fde021ac809972b14bb81a7e4e2ed
yunna <homer_j@hanmail.net>
2023-03-30 19:39:46 +0900

    🚧 progress : slick 테스트

--- src/screen/Tutorial/components/Result.tsx
+++ src/screen/Tutorial/components/Result.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const Result = () => {
   return (
