9af5003aa52087a8fe841577a136c981a9e36d12
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-03-31 12:11:25 +0900

    e

---
+++ dist/assets/index-ae6e8262.js
@@ -0,0 +1,42 @@
+import{a as e,j as n,s as t}from"./index-f57bf967.js";const s=()=>e(i,{children:n(l,{children:[e(o,{children:"어떤 시설이 필요하신가요?"}),n(c,{children:[e("label",{htmlFor:"all",children:"전체 선택"}),e("input",{type:"checkbox",name:"all",id:"all"})]}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(r,{children:"선택 UI"}),e(d,{children:"다음 페이지로"})]})}),i=t.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid red;
+`,l=t.div`
+  margin: 8rem 2.4rem;
+  border: 1px solid rebeccapurple;
+`,o=t.h1`
+  font-size: 2.5rem;
+  display: flex;
+  justify-content: center;
+  font-weight: 400;
+  line-height: 3.026rem;
+`,c=t.div`
+  font-size: 2.5rem;
+  margin: 1.1rem 0rem;
+  text-align: end;
+  & > label {
+    margin-right: 1.3rem;
+  }
+`,r=t.button`
+  width: 100%;
+  height: 5rem;
+  font-size: 2.5rem;
+  background-color: #efeff0;
+  text-align: start;
+  padding: 1rem;
+  & + & {
+    margin-top: 2.2rem;
+  }
+`,d=t.button`
+  width: 16.6rem;
+  height: 5rem;
+  margin-top: 5.5rem;
+  background-color: white;
+  border: 0.1rem solid black;
+  margin-left: auto;
+  display: block;
+`;export{s as default};
