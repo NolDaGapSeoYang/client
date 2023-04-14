06965682d93adb53a9d57df52fd7fe993bb5666f
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-04-14 15:02:04 +0900

    test: vite rollup 제거

--- vite.config.ts
+++ vite.config.ts
@@ -13,21 +13,6 @@ export default ({ mode }) => {
       port: 3000,
       host: true,
     },
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            let extType = assetInfo.name.split('.').at(1)
-            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-              extType = 'img'
-            }
-            return `assets/${extType}/[name]-[hash][extname]`
-          },
-          chunkFileNames: 'assets/js/[name]-[hash].js',
-          entryFileNames: 'assets/js/[name]-[hash].js',
-        },
-      },
-    },
     resolve: {
       alias: [
         {
