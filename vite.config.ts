3883bb04aed6371d87fbca1fcd789eea11c636c8
Raymond <73725736+raymondanythings@users.noreply.github.com>
2023-04-14 14:26:39 +0900

    test: 배포 테스트

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,21 @@ export default ({ mode }) => {
       port: 3000,
       host: true,
     },
-
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            let extType = assetInfo.name.split('.').at(1)
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'img'
+            }
+            return `assets/${extType}/[name]-[hash][extname]`
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+        },
+      },
+    },
     resolve: {
       alias: [
         {
